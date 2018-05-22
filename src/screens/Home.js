c50018a96febe9b5b194137865c5dc4cceee7378
Arukio <rakamahardika22@gmail.com>
2018-05-22 09:02:15 +0700

    first init

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Text } from "react-native";
+import { Container } from "../components/Container";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Container>
+        <Text>Hallo</Text>
+      </Container>
+    );
+  }
+}
